973ed1766f41abb2a33cadd29087fd8c4499aa74
Jefferson Abraham Omier <71264930+TheSteelNinjaCode@users.noreply.github.com>
2026-08-02 06:37:16 -0600

    Refactor HTML layout in layout.py for improved readability and update version to 1.3.1 in package.json

--- dist/src/app/layout.py
+++ dist/src/app/layout.py
@@ -2,31 +2,28 @@ def layout():
     return r"""
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{ metadata.title | default("Caspian App") }}</title>
-    <meta
-      name="description"
-      content="{{ metadata.description | default('Powered by Caspian Framework') }}"
-    />
-    {% if metadata.robots %}
-    <meta name="robots" content="{{ metadata.robots }}" />
-    {% endif %}
-    <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16" />
-    <link href="/css/styles.css" rel="stylesheet" />
-    <script type="module" src="/js/main.js"></script>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport"
+        content="width=device-width, initial-scale=1.0" />
+  <title>{{ metadata.title | default("Caspian App") }}</title>
+  <meta name="description"
+        content="{{ metadata.description | default('Powered by Caspian Framework') }}" />
+  <link rel="icon"
+        href="/favicon.ico"
+        type="image/x-icon"
+        sizes="16x16" />
 </head>
 
-  <body
-    style="
+<body style="
       opacity: 0;
       pointer-events: none;
       user-select: none;
       transition: opacity 0.18s ease-out;
-    "
-  >
-    <slot />
-  </body>
+    ">
+  <slot />
+</body>
+
 </html>
 """
